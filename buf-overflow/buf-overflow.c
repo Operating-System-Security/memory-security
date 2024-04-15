200b410eaf3e9657d0d20b78e0f0218cbb439781
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2024-04-15 21:15:48 +0800

    buf-overflow: inject shellcode using overflow exploit

--- buf-overflow/buf-overflow.c
+++ buf-overflow/buf-overflow.c
@@ -1,27 +1,16 @@
 #include <stdio.h>
 #include <string.h>
-#include <err.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-	char buf[2048];
-	int len = 0;
-	if (!fgets(buf, sizeof(buf), stdin))
-		err(1, "Too long input");
+	char buf[128];		// buf == ebp - 0x88
+	char input[2048];
+	printf("%p\n", buf);
+	printf("%p\n", input);
 
-	// a few info for debugging
-	while (buf[len++] != '\n');
-	printf("> shellcode length: %d\n", len);
-	printf("> shellcode content:\n");
-	for (int i = 0; i < len; i += 1) {
-		if (i % 16 == 0)
-			printf("\t%04X: ", i);
-		printf("%02X ", (unsigned char)buf[i]);
-		if (i % 16 == 15)
-			printf("\n");
-	}
-	printf("\n");
+	if (!fgets(input, sizeof(input), stdin))
+		return -1;
 
-	(*(void (*)()) buf)();
+	strcpy(buf, input);
 }
